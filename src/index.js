38b3b13eb9577074103607129aeda6b789fdc683
Prince SBI <shreyasgs403@gmail.com>
2023-01-09 23:04:25 +0530

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+// import Hello from './Hello';
+// import Card from './Card';
+// import CardList from './CardList';
 import reportWebVitals from './reportWebVitals';
+import App from './containers/App';
+import 'tachyons'
+// import { robots } from './robots';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    {/* <Hello greeting={'Hello React Ninja'} />
+    <App />
+    <h1>Good Bye World</h1> */}
+    {/* <Card id={robots[0].id} name={robots[0].name} email={robots[0].email}/>
+    <Card id={robots[1].id} name={robots[1].name} email={robots[1].email}/>
+    <Card id={robots[2].id} name={robots[2].name} email={robots[2].email}/>
+    <Card id={robots[3].id} name={robots[3].name} email={robots[3].email}/> */}
+    {/* <CardList robots={robots}/> */}
     <App />
   </React.StrictMode>
 );
